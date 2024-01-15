71035dab1e33c26519355621bca2af7a41f8d3b1
Ilyas Shagaleev <ilyas1996shagaleev@gmail.com>
2024-01-15 23:48:17 +0500

    Test for get created user

---
+++ src/tests/getCreatedObject.test.ts
@@ -0,0 +1,48 @@
+import { test, expect } from '@playwright/test';
+import { API_KEY } from '../../api_key';
+import { urls } from '../common/urls';
+import { dataRequest } from '../common/data';
+
+let uuid: string
+
+test.afterEach('Deleting created object',async({request})=>{
+     await request.delete(`${urls.main}user`,{
+        headers: {
+            Authorization: `Bearer ${API_KEY}`,
+          },
+          data:`[{"_uuid": "${uuid}"}]`
+        })   
+})
+
+
+test('Geting data of created object user Ivan', async({request})=>{
+    await request.post(`${urls.main}user`,{
+    headers: {
+        Authorization: `Bearer ${API_KEY}`,
+      },
+      data: dataRequest.user
+    }
+)
+    
+    let res = await request.get(`${urls.main}user`,{
+        headers: {
+            Authorization: `Bearer ${API_KEY}`,
+          }
+    
+        }
+    )
+    let resJson = await res.json()
+    uuid = await resJson.items[0]._uuid
+    await expect(res).toBeOK()
+    await expect(res.status()).toBe(200)
+    await expect(resJson.items[0]).toBeTruthy()
+    await expect(resJson.items[0]._created).toBeTruthy()
+    await expect(resJson.items[0]._data_type).toBeTruthy()
+    await expect(resJson.items[0]._is_deleted).toBe(false)
+    await expect(resJson.items[0]._modified).toBeTruthy()
+    await expect(resJson.items[0]._self_link).toBe(`${urls.base}${urls.main}user/${resJson.items[0]._uuid}`)
+    await expect(resJson.items[0]._user).toBeTruthy()
+    await expect(resJson.items[0]._uuid).toBeTruthy()
+    await expect(resJson.items[0].city).toBe('Moscow')
+    await expect(resJson.items[0].name).toBe('Ivan')
+})
