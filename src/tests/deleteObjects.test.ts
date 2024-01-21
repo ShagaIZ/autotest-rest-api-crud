671695e231959f4a92f8245f1bfe280a5b94c0c6
Ilyas Shagaleev <ilyas1996shagaleev@gmail.com>
2024-01-21 19:12:10 +0500

    Updated test

--- src/tests/deleteObjects.test.ts
+++ src/tests/deleteObjects.test.ts
@@ -3,17 +3,27 @@ import { API_KEY } from '../../api_key'
 import { urls } from '../common/urls'
 import { dataRequest } from '../common/data'
 
-let uuid: string
+let uuidUserOne: string
+let uuidUserTwo: string
 
 test.beforeEach('Creating object', async ({ request }) => {
-	let res = await request.post(`${urls.main}user`, {
+	let resCreateUserOne = await request.post(`${urls.main}user`, {
 		headers: {
 			Authorization: `Bearer ${API_KEY}`,
 		},
 		data: dataRequest.userOne,
 	})
-	let resJson = await res.json()
-	uuid = await resJson.items[0]._uuid
+	let resCreateUserTwo = await request.post(`${urls.main}user`, {
+		headers: {
+			Authorization: `Bearer ${API_KEY}`,
+		},
+		data: dataRequest.userTwo,
+	})
+
+	let resjsonCreateUserOne = await resCreateUserOne.json()
+	let resjsonCreateUserTwo = await resCreateUserTwo.json()
+	uuidUserOne = await resjsonCreateUserOne.items[0]._uuid
+	uuidUserTwo = await resjsonCreateUserTwo.items[0]._uuid
 })
 
 test.afterEach('Creating object', async ({ request }) => {
@@ -21,31 +31,59 @@ test.afterEach('Creating object', async ({ request }) => {
 		headers: {
 			Authorization: `Bearer ${API_KEY}`,
 		},
-		data: `[{"_uuid": "${uuid}"}]`,
+		data: `[{"_uuid": "${uuidUserOne}"}]`,
+	})
+	await request.delete(`${urls.main}user`, {
+		headers: {
+			Authorization: `Bearer ${API_KEY}`,
+		},
+		data: `[{"_uuid": "${uuidUserTwo}"}]`,
 	})
 })
 test.describe('Deleting objects', async () => {
-	test('Valid url and data, with token -> object deleted', async ({ request }) => {
-		let resDelete = await request.delete(`${urls.main}user`, {
+	test('Valid url and data, with token -> object deleteds', async ({ request }) => {
+		let resDeleteUserOne = await request.delete(`${urls.main}user`, {
 			headers: {
 				Authorization: `Bearer ${API_KEY}`,
 			},
-			data: `[{"_uuid": "${uuid}"}]`,
+			data: `[{"_uuid": "${uuidUserOne}"}]`,
 		})
-		let resDeleteJson = await resDelete.json()
-		await expect(resDelete).toBeOK()
-		await expect(resDelete.status()).toBe(200)
-		await expect(resDeleteJson.items[0]).toBeTruthy()
-		await expect(resDeleteJson.items[0]._created).toBeTruthy()
-		await expect(resDeleteJson.items[0]._data_type).toBeTruthy()
-		await expect(resDeleteJson.items[0]._is_deleted).toBe(true)
-		await expect(resDeleteJson.items[0]._modified).toBeTruthy()
-		await expect(resDeleteJson.items[0]._self_link).toBe(`${urls.base}${urls.main}user/${resDeleteJson.items[0]._uuid}`)
-		await expect(resDeleteJson.items[0]._user).toBeTruthy()
-		await expect(resDeleteJson.items[0]._uuid).toBeTruthy()
-		await expect(resDeleteJson.items[0].city).toBe('Moscow')
-		await expect(resDeleteJson.items[0].name).toBe('Ivan')
-		await expect(resDeleteJson.items[0].age).toBe(25)
+		let resDeleteUserOneJson = await resDeleteUserOne.json()
+		let resDeleteUserTwo = await request.delete(`${urls.main}user`, {
+			headers: {
+				Authorization: `Bearer ${API_KEY}`,
+			},
+			data: `[{"_uuid": "${uuidUserTwo}"}]`,
+		})
+		let resDeleteUserTwoJson = await resDeleteUserOne.json()
+		//First block assertation
+		await expect(resDeleteUserOne).toBeOK()
+		await expect(resDeleteUserOne.status()).toBe(200)
+		await expect(resDeleteUserOneJson.items[0]).toBeTruthy()
+		await expect(resDeleteUserOneJson.items[0]._created).toBeTruthy()
+		await expect(resDeleteUserOneJson.items[0]._data_type).toBeTruthy()
+		await expect(resDeleteUserOneJson.items[0]._is_deleted).toBe(true)
+		await expect(resDeleteUserOneJson.items[0]._modified).toBeTruthy()
+		await expect(resDeleteUserOneJson.items[0]._self_link).toBe(`${urls.base}${urls.main}user/${resDeleteUserOneJson.items[0]._uuid}`)
+		await expect(resDeleteUserOneJson.items[0]._user).toBeTruthy()
+		await expect(resDeleteUserOneJson.items[0]._uuid).toBeTruthy()
+		await expect(resDeleteUserOneJson.items[0].city).toBe('Moscow')
+		await expect(resDeleteUserOneJson.items[0].name).toBe('Ivan')
+		await expect(resDeleteUserOneJson.items[0].age).toBe(25)
+		//Second block assertation
+		await expect(resDeleteUserTwo).toBeOK()
+		await expect(resDeleteUserTwo.status()).toBe(200)
+		await expect(resDeleteUserTwoJson.items[0]).toBeTruthy()
+		await expect(resDeleteUserTwoJson.items[0]._created).toBeTruthy()
+		await expect(resDeleteUserTwoJson.items[0]._data_type).toBeTruthy()
+		await expect(resDeleteUserTwoJson.items[0]._is_deleted).toBe(true)
+		await expect(resDeleteUserTwoJson.items[0]._modified).toBeTruthy()
+		await expect(resDeleteUserTwoJson.items[0]._self_link).toBe(`${urls.base}${urls.main}user/${resDeleteUserOneJson.items[0]._uuid}`)
+		await expect(resDeleteUserTwoJson.items[0]._user).toBeTruthy()
+		await expect(resDeleteUserTwoJson.items[0]._uuid).toBeTruthy()
+		await expect(resDeleteUserTwoJson.items[0].city).toBe('Moscow')
+		await expect(resDeleteUserTwoJson.items[0].name).toBe('Ivan')
+		await expect(resDeleteUserTwoJson.items[0].age).toBe(25)
 	})
 
 	test('Invaild url -> 404 error', async ({ request }) => {
@@ -53,7 +91,7 @@ test.describe('Deleting objects', async () => {
 			headers: {
 				Authorization: `Bearer ${API_KEY}`,
 			},
-			data: `[{"_uuid": "${uuid}"}]`,
+			data: `[{"_uuid": "${uuidUserOne}"}]`,
 		})
 		await expect(resDelete.status()).toBe(404)
 	})
@@ -75,7 +113,7 @@ test.describe('Deleting objects', async () => {
 			headers: {
 				Authorization: `Bearer ${API_KEY}`,
 			},
-			data: `[{"_uuid": "${uuid}"}]`,
+			data: `[{"_uuid": "${uuidUserTwo}"}]`,
 		})
 		await expect(resDelete.status()).toBe(400)
 	})
@@ -85,7 +123,7 @@ test.describe('Deleting objects', async () => {
 			headers: {
 				Authorization: ``,
 			},
-			data: `[{"_uuid": "${uuid}"}]`,
+			data: `[{"_uuid": "${uuidUserOne}"}]`,
 		})
 		let resJson = await resDelete.json()
 		await expect(resJson.error).toBe('Bad request')
